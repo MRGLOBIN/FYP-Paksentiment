f24eee9df61bb16df34b4b472c4f2d218ca34b93
Muhammad-Usman-Malik <usmanmalik0926@gmail.com>
2026-03-06 15:51:09 +0500

    add PaymentModal, Forgot Password and Pricing pages with styles

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,25 +1,27 @@
-import HeroSection from './components/HeroSection'
-import StatsSection from './components/StatsSection'
-import FeaturesSection from './components/FeaturesSection'
-import DataSourcesSection from './components/DataSourcesSection'
-import HowItWorksSection from './components/HowItWorksSection'
-import PrivacySection from './components/PrivacySection'
-import CTASection from './components/CTASection'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import HeroSection from "./components/HeroSection";
+import StatsSection from "./components/StatsSection";
+import FeaturesSection from "./components/FeaturesSection";
+import DataSourcesSection from "./components/DataSourcesSection";
+import HowItWorksSection from "./components/HowItWorksSection";
+import PrivacySection from "./components/PrivacySection";
+import CTASection from "./components/CTASection";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <HeroSection />
-      <StatsSection />
-      <FeaturesSection />
-      <DataSourcesSection />
-      <HowItWorksSection />
-      <PrivacySection />
-      <CTASection />
+      <main>
+        <HeroSection />
+        <StatsSection />
+        <FeaturesSection />
+        <DataSourcesSection />
+        <HowItWorksSection />
+        <PrivacySection />
+        <CTASection />
+      </main>
       <Footer />
     </>
-  )
+  );
 }
